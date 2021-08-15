13a66fa5f270610f54da13dc8ff9ae1b1d51e58a
bda605 <bda605@gmail.com>
2021-08-16 02:04:08 +0800

    Update UserServiceTest.cs

--- ManagerTest/UserServiceTest.cs
+++ ManagerTest/UserServiceTest.cs
@@ -15,7 +15,7 @@ namespace ManagerTest
         [Test]
         public void LoginSuccess()
         {
-            UserResponseViewModel expected = SetExpected(true, "老大", "ADMIN");
+            UserResponseViewModel expected = SetExpected(true, "ADMIN", "ADMIN");
             var userService = new FakeUserService();
             var result = userService.Login("ADMIN", "ADMIN");
             expected.Should().BeEquivalentTo(result);
@@ -57,7 +57,7 @@ namespace ManagerTest
         {
             if (id == "ADMIN" && pwd == "ADMIN") 
             {
-                return new UserViewModel { Name = "老大", RoleId = 1 };
+                return new UserViewModel { Name = "礎��瞻j", RoleId = 1 };
             }
             return null;
         }
